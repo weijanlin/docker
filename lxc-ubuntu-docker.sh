9c6e77f5171a465217a3cdc8d9c5b36d389e94d4
WEIJAN LIN <weijanlin@gmail.com>
2021-11-05 22:10:23 +0800

    Update lxc-ubuntu-docker.sh

--- lxc-ubuntu-docker.sh
+++ lxc-ubuntu-docker.sh
@@ -3,6 +3,8 @@ sudo apt upgrade -y
 sudo apt install gnupg2 -y
 sudo apt install curl -y
 sudo apt install software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(cat /etc/os-release | grep UBUNTU_CODENAME | cut -d '=' -f 2) stable"
-sudo apt update -y
-sudo apt install docker-ce -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
